522f7edb8725b07fb278912e70be7fd2597a8d5b
yfge <geyunfei@gmail.com>
2025-12-17 11:22:10 +0800

    chore(backend): merge alembic heads

---
+++ ai-pic-backend/alembic/versions/merge_f2d1a3c4b5e6_1c2d3e4f5a67.py
@@ -0,0 +1,17 @@
+"""merge heads f2d1a3c4b5e6 and 1c2d3e4f5a67"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "b7f9e6d3c2a1"
+down_revision = ("f2d1a3c4b5e6", "1c2d3e4f5a67")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
